104810ece5bda6229e53bcc6e95e2595c928fd21
‘angeynik’ <angeynik@gmail.com>
2024-08-09 16:57:10 +0300

    ecosystem.config.js added

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  serverhttp : [{
+    script: 'dist/http-server',
+    watch: 'dist',
+watch_delay: 5000
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
